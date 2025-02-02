4457698a5e6bb58579041dad76ff297b421c3905
Islam <islam.ayoub@swift-cloud-solutions.com>
2025-02-02 17:32:19 +0200

    would this work

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 3000;
+
+const homepage = require('../routes/home.js');
 
 // Expect JSON or Form Data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(homepage);
 
 
 
