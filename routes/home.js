dbe7986e2aa15c4d6eaab6cbb9d3f48eaaa5482e
Islam <islam.ayoub@swift-cloud-solutions.com>
2025-02-02 23:41:18 +0200

    auth

--- routes/home.js
+++ routes/home.js
@@ -13,24 +13,24 @@ const authParams = {
     }
 }
 
-router.get('/', (req, res) => {
-    console.log('hello from home');
-    console.log("Inside home router:", req.method, req.url);
-
-    res.send('hello from home');
-})
-
-// router.get('/', async (req, res, next) => {
-//         try {
-//             const response = await axios.post(authURL, null, authParams);
-//             req.access_token = response.data.access_token;
-//             console.log('Hello from Homepage')
-//             console.log(`Authenticated with token ${response.data.access_token}`);
-//             next();
-//         } catch (error) {
-//             console.log(`Authentication error: ${error.message}`);
-//             res.status(500).send(error.message);
-//         }
+// router.get('/', (req, res) => {
+//     console.log('hello from home');
+//     console.log("Inside home router:", req.method, req.url);
+//
+//     res.send('hello from home');
 // })
+
+router.get('/', async (req, res, next) => {
+        try {
+            const response      = await axios.post(authURL, null, authParams);
+            req.access_token    = response.data.access_token;
+            console.log('Hello from Homepage')
+            console.log(`Authenticated with token ${response.data.access_token}`);
+            next();
+        } catch (error) {
+            console.log(`Authentication error: ${error.message}`);
+            res.status(500).send(error.message);
+        }
+})
 
 module.exports = router;
