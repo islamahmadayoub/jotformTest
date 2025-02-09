563002a3ccb6668ca4995d9fddc1ca56db51475a
Islam <islam.ayoub@swift-cloud-solutions.com>
2025-02-09 05:47:25 +0200

    is this it?

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 /* --------------------------------------- */
 // Setup
-const express = require('express');
-const jsforce = require('jsforce');
+const express   = require('express');
+const jsforce   = require('jsforce');
+const axios     = require('axios');
 
 
-const index = express();
-const port = process.env.PORT || 3000;
+const index     = express();
+const port      = process.env.PORT || 3000;
+
+const SALESFORCE_DOMAIN = process.env.SALESFORCE_URL;
+const CLIENT_ID         = process.env.SALESFORCE_CLIENT_ID;
+const CLIENT_SECRET     = process.env.SALESFORCE_CLIENT_SECRET;
 
 const connectionObject = {
     instanceUrl     : process.env.SALESFORCE_URL,
@@ -27,8 +32,29 @@ index.get('/', (req, res, next) => {
     next();
 });
 
+// index.use( async (req, res) => {
+//     try {
+//         const response = await axios.post(`${SALESFORCE_DOMAIN}/services/oauth2/token`, null, {
+//             params: {
+//                 grant_type: "client_credentials",
+//                 client_id: CLIENT_ID,
+//                 client_secret: CLIENT_SECRET
+//             },
+//             headers: {
+//                 "Content-Type": "application/x-www-form-urlencoded"
+//             }
+//         });
+//
+//         res.json(response.data);
+//     } catch (error) {
+//         console.error("Error fetching token:", error.response?.data || error.message);
+//         res.status(500).json({ error: "Failed to retrieve access token" });
+//     }
+// });
+
 index.use(async (req, res) => {
     console.log('CUSTOM MESSAGE ::: Auth is initiated');
+    console.log(`client id is ::: ${connectionObject.oauth.clientId}`);
     const conn = new jsforce.Connection(connectionObject)
     const userInfo = await conn.authorize({ grant_type: "client_credentials" });
     console.log(`Access Token is ::: ${conn.accessToken}`);
